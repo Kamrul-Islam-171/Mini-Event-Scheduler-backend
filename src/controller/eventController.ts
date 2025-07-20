2dbdfcafa6a52d23cba2a5c8a931b0cd7a6f93b0
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2025-07-20 23:18:35 +0600

    fixed Cannot set headers after they are sent to the client

--- src/controller/eventController.ts
+++ src/controller/eventController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { TEvent } from '../models/eventModel';
 import { Categorize } from '../utils/categorize';
 
-
 let events: TEvent[] = [];
 let index = 1;
 const AddEvents = async (req: Request, res: Response) => {
@@ -25,13 +24,13 @@ const AddEvents = async (req: Request, res: Response) => {
     events.push(newEvent);
     index += 1;
     // console.log(events)
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: 'Event created Successfully',
       data: newEvent,
     });
   } catch (error: any) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error?.message,
     });
   }
@@ -41,19 +40,19 @@ const GetEvents = async (req: Request, res: Response) => {
     const sortedEvents = [...events].sort((a, b) => {
       const dateA = new Date(`${a.date}T${a.time}`);
       const dateB = new Date(`${b.date}T${b.time}`);
-      console.log(dateA, dateB);
-      return dateB.getTime() - dateA.getTime();
+      // console.log(dateA, dateB);
+      return dateA.getTime() - dateB.getTime();
     });
 
     // console.log(sortedEvents)
 
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: 'Events retrive Successfully',
       data: sortedEvents,
     });
   } catch (error: any) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error?.message,
     });
   }
@@ -61,46 +60,47 @@ const GetEvents = async (req: Request, res: Response) => {
 const UpdateEvent = async (req: Request, res: Response) => {
   try {
     const id = parseInt(req.params.id);
-    const event = events.filter((e) => e.id == id);
-    if (!event) {
-      res.status(404).json({
+    // console.log("id = ", id)
+    let event = events.filter((e) => e.id == id);
+    if (!event[0]) {
+      return res.status(404).json({
         message: 'Event not found!',
       });
     }
 
-    events[id - 1].archived = true;
+    // console.log(event)
 
-    res.status(201).json({
+    event[0].archived = true;
+
+    return res.status(201).json({
       success: true,
       message: 'Events updated Successfully',
-      data: events[id - 1],
+      data: event[0],
     });
   } catch (error: any) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error?.message,
     });
   }
 };
 const DeleteEvent = async (req: Request, res: Response) => {
   try {
-    
     const id = parseInt(req.params.id);
     const event = events.filter((e) => e.id == id);
-    if (!event) {
-      res.status(404).json({
+    if (!event[0]) {
+      return res.status(404).json({
         message: 'Event not found!',
       });
     }
 
     events = [...events].filter((e) => e.id !== id);
 
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: 'Events deleted Successfully',
-      
     });
   } catch (error: any) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error?.message,
     });
   }
@@ -110,5 +110,5 @@ export const EventController = {
   AddEvents,
   GetEvents,
   UpdateEvent,
-  DeleteEvent
+  DeleteEvent,
 };
