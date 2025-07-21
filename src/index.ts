3485d26ba91d8d43db4691945a31d89628338a34
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2025-07-21 15:52:07 +0600

    cors added

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import { EventRoutes } from './routes/eventRoute';
 import dotenv from 'dotenv'
 import globalErrorHandler from './middleware/globalErrorHandeler';
 import notFound from './errors/notFound';
-
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-
+app.use(cors({
+  origin: ['http://localhost:5173']
+}))
 app.use('/api', EventRoutes);
 
 
